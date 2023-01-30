e1c86baec1f602fd00ad32f6a12754775ff6a509
Manuel Ortiz Hernández <manuoh09@gmail.com>
2023-01-30 16:10:21 -0600

    Navbar added

---
+++ frontend/src/components/common/Header.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
+import './styles/Header.css'
+
+const Header = () => {
+    return(
+        <Navbar bg="dark" expand="lg">
+            <Container>
+                <Navbar.Brand className="text-white text-uppercase font-weight-bold">Ideas S.A de C.V</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link >
+                        <li className="nav-item ">
+                            <Link to="/" className="nav-link text-white">Inicio</Link>
+                        </li>
+                    </Nav.Link>
+                    <Nav.Link >
+                        <li className="nav-item ">
+                            <Link to="/warehouses" className="nav-link text-white">Almacenes</Link>
+                        </li>
+                    </Nav.Link>
+                    <li className="nav-item text-white">
+                        <NavDropdown className="text-white mt-2" title={<span className="text-white ">Mercancía</span>} id="basic-nav-dropdown">
+                            <NavDropdown.Item>
+                                <Link to="/inventaries" className="nav-link text-black">Inventarios</Link>
+                            </NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item>
+                                <Link to="/orders" className="nav-link text-black">Órdenes de Compra</Link>
+                            </NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item>
+                                <Link to="/receptions" className="nav-link text-black">Recepción de Mercancía</Link>
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </li>
+
+
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Header;
