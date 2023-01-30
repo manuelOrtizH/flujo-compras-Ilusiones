5945768bf6c031a95e4880752352fd85c303d6ca
Manuel Ortiz Hernández <manuoh09@gmail.com>
2023-01-29 19:59:40 -0600

    Update models.py

--- backend/products/models.py
+++ backend/products/models.py
@@ -2,6 +2,7 @@ from djongo import models
 
 # Create your models here.
 class Inventory(models.Model): #Inventario
+    sub = models.CharField(max_length=100)
     model = models.CharField(max_length=200)
     invoice = models.CharField(max_length=50)
     imei = models.CharField(max_length=25)
